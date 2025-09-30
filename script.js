e0d2a85eb677050dadbde8b43c6050a9b58714ea
MiniBats <mesethan1@hotmail.com>
2025-09-30 22:03:41 +0200

    ajout des boutons + review navbar

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("cv").addEventListener("click", function() {
+  window.open("CV.pdf", "_blank");
+});
+
+document.getElementById("motivation").addEventListener("click", function() {
+  window.open("lettre_motivation.pdf", "_blank");
+});
